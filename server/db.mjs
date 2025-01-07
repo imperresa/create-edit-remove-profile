5da8fc02036bc990e296dd4506622b0ae909c0ff
nueng <166599504+imperresa@users.noreply.github.com>
2025-01-07 18:26:50 +0700

    Update db.mjs

--- server/db.mjs
+++ server/db.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-let connectionString ='mongodb://localhost:27017/'
+let connectionString ='mongodb://localhost:27017/' //ใส่ connectionString mongodb ตรงนี้
 
 export const client = new MongoClient(connectionString)
 
